35554719feb8355bb48f19cdd8c167a5e4adf9cd
www_ <aye@sensolabs.com>
2023-11-28 11:04:20 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import Home from "./pages/Home";
+
 const App = () => {
-  return <div>HHHH</div>;
+  return <Home/>;
 };
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const slideItems = [
+    {
+        id: 1,
+        img: "/assets/pink-items.png",
+        title: "WENESDAY PINK SALE!",
+        desc: "CHECK ON PINK ITEMS.",
+        bg: "",
+    },
+    {
+        id: 2,
+        img: "/assets/new.jpg",
+        title: "NEW SALE",
+        desc: "GET FLAT 50% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 3,
+        img: "https://shorturl.at/ikvK4",
+        title: "POPULAR SALE",
+        desc: "GET FLAT 50% OFF FOR POPULAR ITEMS.",
+        bg: "f9e0db",
+    }
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "/assets/decoration.jpeg",
+        title: "DECORATION!",
+    },
+    {
+        id: 2,
+        img: "/assets/mixnmatch.png",
+        title: "MIX AND LOVE",
+    },
+    {
+        id: 3,
+        img: "/assets/plant.png",
+        title: "PLANT WORLD",
+    },
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "assets/mubox-pink.png",
+    },
+    {
+        id: 2,
+        img: "assets/lamp-pink.png",
+    },
+    {
+        id: 3,
+        img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+        id: 4,
+        img: "assets/india-plant.png",
+    },
+    {
+        id: 5,
+        img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+        id: 6,
+        img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+        id: 7,
+        img: "assets/monstera.png",
+    },
+    {
+        id: 8,
+        img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+];
